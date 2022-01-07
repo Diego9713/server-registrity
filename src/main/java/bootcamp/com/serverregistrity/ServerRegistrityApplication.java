eb47e535194f18a28649cc83a35fc55c3a463a25
Diego Santiago Tafur Sanchez <dtafursa@emeal.nttdata.com>
2022-01-06 23:58:27 -0500

    hotfix server-registrity

---
+++ src/main/java/bootcamp/com/serverregistrity/ServerRegistrityApplication.java
@@ -0,0 +1,15 @@
+package bootcamp.com.serverregistrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerRegistrityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRegistrityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/com/serverregistrity/ServerRegistrityApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.com.serverregistrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
